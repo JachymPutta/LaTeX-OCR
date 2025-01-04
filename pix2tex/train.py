1a8c34edea80fafce0d03eecdb2b7801da88bf65
Jachym Putta <jachym.putta@gmail.com>
2025-01-04 16:37:09 +0000

    feat: initial commit -- fixes

--- pix2tex/train.py
+++ pix2tex/train.py
@@ -11,8 +11,16 @@ import wandb
 import torch.nn as nn
 from pix2tex.eval import evaluate
 from pix2tex.models import get_model
+
 # from pix2tex.utils import *
-from pix2tex.utils import in_model_path, parse_args, seed_everything, get_optimizer, get_scheduler, gpu_memory_check
+from pix2tex.utils import (
+    in_model_path,
+    parse_args,
+    seed_everything,
+    get_optimizer,
+    get_scheduler,
+    gpu_memory_check,
+)
 
 
 def train(args):
@@ -34,13 +42,18 @@ def train(args):
         model.load_state_dict(torch.load(args.load_chkpt, map_location=device))
 
     def save_models(e, step=0):
-        torch.save(model.state_dict(), os.path.join(out_path, '%s_e%02d_step%02d.pth' % (args.name, e+1, step)))
-        yaml.dump(dict(args), open(os.path.join(out_path, 'config.yaml'), 'w+'))
+        torch.save(
+            model.state_dict(),
+            os.path.join(out_path, "%s_e%02d_step%02d.pth" % (args.name, e + 1, step)),
+        )
+        yaml.dump(dict(args), open(os.path.join(out_path, "config.yaml"), "w+"))
 
     opt = get_optimizer(args.optimizer)(model.parameters(), args.lr, betas=args.betas)
-    scheduler = get_scheduler(args.scheduler)(opt, step_size=args.lr_step, gamma=args.gamma)
+    scheduler = get_scheduler(args.scheduler)(
+        opt, step_size=args.lr_step, gamma=args.gamma
+    )
 
-    microbatch = args.get('micro_batchsize', -1)
+    microbatch = args.get("micro_batchsize", -1)
     if microbatch == -1:
         microbatch = args.batchsize
 
@@ -53,25 +66,43 @@ def train(args):
                     opt.zero_grad()
                     total_loss = 0
                     for j in range(0, len(im), microbatch):
-                        tgt_seq, tgt_mask = seq['input_ids'][j:j+microbatch].to(device), seq['attention_mask'][j:j+microbatch].bool().to(device)
-                        loss = model.data_parallel(im[j:j+microbatch].to(device), device_ids=args.gpu_devices, tgt_seq=tgt_seq, mask=tgt_mask)*microbatch/args.batchsize
+                        tgt_seq, tgt_mask = (
+                            seq["input_ids"][j : j + microbatch].to(device),
+                            seq["attention_mask"][j : j + microbatch].bool().to(device),
+                        )
+                        loss = (
+                            model.data_parallel(
+                                im[j : j + microbatch].to(device),
+                                device_ids=args.gpu_devices,
+                                tgt_seq=tgt_seq,
+                                mask=tgt_mask,
+                            )
+                            * microbatch
+                            / args.batchsize
+                        )
                         loss.backward()  # data parallism loss is a vector
                         total_loss += loss.item()
                         torch.nn.utils.clip_grad_norm_(model.parameters(), 1)
                     opt.step()
                     scheduler.step()
-                    dset.set_description('Loss: %.4f' % total_loss)
+                    dset.set_description("Loss: %.4f" % total_loss)
                     if args.wandb:
-                        wandb.log({'train/loss': total_loss})
-                if (i+1+len(dataloader)*e) % args.sample_freq == 0:
-                    bleu_score, edit_distance, token_accuracy = evaluate(model, valdataloader, args, num_batches=int(args.valbatches*e/args.epochs), name='val')
+                        wandb.log({"train/loss": total_loss})
+                if (i + 1 + len(dataloader) * e) % args.sample_freq == 0:
+                    bleu_score, edit_distance, token_accuracy = evaluate(
+                        model,
+                        valdataloader,
+                        args,
+                        num_batches=int(args.valbatches * e / args.epochs),
+                        name="val",
+                    )
                     if bleu_score > max_bleu and token_accuracy > max_token_acc:
                         max_bleu, max_token_acc = bleu_score, token_accuracy
                         save_models(e, step=i)
-            if (e+1) % args.save_freq == 0:
+            if (e + 1) % args.save_freq == 0:
                 save_models(e, step=len(dataloader))
             if args.wandb:
-                wandb.log({'train/epoch': e+1})
+                wandb.log({"train/epoch": e + 1})
     except KeyboardInterrupt:
         if e >= 2:
             save_models(e, step=i)
@@ -79,24 +110,30 @@ def train(args):
     save_models(e, step=len(dataloader))
 
 
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Train model')
-    parser.add_argument('--config', default=None, help='path to yaml config file', type=str)
-    parser.add_argument('--no_cuda', action='store_true', help='Use CPU')
-    parser.add_argument('--debug', action='store_true', help='DEBUG')
-    parser.add_argument('--resume', help='path to checkpoint folder', action='store_true')
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Train model")
+    parser.add_argument(
+        "--config", default=None, help="path to yaml config file", type=str
+    )
+    parser.add_argument("--no_cuda", action="store_true", help="Use CPU")
+    parser.add_argument("--debug", action="store_true", help="DEBUG")
+    parser.add_argument(
+        "--resume", help="path to checkpoint folder", action="store_true"
+    )
     parsed_args = parser.parse_args()
     if parsed_args.config is None:
         with in_model_path():
-            parsed_args.config = os.path.realpath('settings/debug.yaml')
-    with open(parsed_args.config, 'r') as f:
+            parsed_args.config = os.path.realpath("settings/debug.yaml")
+    with open(parsed_args.config, "r") as f:
         params = yaml.load(f, Loader=yaml.FullLoader)
     args = parse_args(Munch(params), **vars(parsed_args))
-    logging.getLogger().setLevel(logging.DEBUG if parsed_args.debug else logging.WARNING)
+    logging.getLogger().setLevel(
+        logging.DEBUG if parsed_args.debug else logging.WARNING
+    )
     seed_everything(args.seed)
     if args.wandb:
         if not parsed_args.resume:
             args.id = wandb.util.generate_id()
-        wandb.init(config=dict(args), resume='allow', name=args.name, id=args.id)
+        wandb.init(config=dict(args), resume="allow", name=args.name, id=args.id)
         args = Munch(wandb.config)
     train(args)
